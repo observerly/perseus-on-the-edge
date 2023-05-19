1904b453cf8717170377f5bc19b5deb0c82ab67a
Michael J. Roberts <michael@observerly.com>
2023-05-19 17:05:36 +0100

    ci: Added runtimeconfig env variables for database connection.
    ci: Added runtimeconfig env variables for database connection. Includes adding env.example for base environment configuration.

--- nitro.config.ts
+++ nitro.config.ts
@@ -12,5 +12,11 @@ export default defineNitroConfig({
         'x-perseus-api-version': 'v1.0.0@latest (2022-06-24)'
       }
     }
+  },
+  runtimeConfig: {
+    db: {
+      url: 'file:db.perseus.sqlite',
+      authToken: ''
+    }
   }
 })
