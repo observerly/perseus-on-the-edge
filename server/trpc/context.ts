218a1b9d85b49537029b4db7ed36f61ff814226b
Michael J. Roberts <michael@observerly.com>
2023-05-21 14:15:52 +0100

    feat: Added createContext = async (_event: H3Event) tRPC event handler.
    feat: Added createContext = async (_event: H3Event) tRPC event handler to inject the db into the t.middleware({ ctx }).

---
+++ server/trpc/context.ts
@@ -0,0 +1,46 @@
+/*****************************************************************************************************************/
+
+// @author         Michael Roberts <michael@observerly.com>
+// @package        @observerly/perseus-on-the-edge
+// @license        Copyright © 2021-2023 observerly
+
+/*****************************************************************************************************************/
+
+import { type H3Event } from 'h3'
+
+import { type inferAsyncReturnType } from '@trpc/server'
+
+import { type LibSQLDatabase } from 'drizzle-orm/libsql'
+
+import { useDb } from '../db/utils'
+
+/*****************************************************************************************************************/
+
+declare module 'h3' {
+  interface H3EventContext {
+    db: LibSQLDatabase
+  }
+}
+
+/*****************************************************************************************************************/
+
+/**
+ * createContext
+ *
+ * @description Create the context for the base tRPC router to use for all routes
+ * @param {H3Event} event
+ *
+ */
+export const createContext = async (_event: H3Event) => {
+  const { db } = useRuntimeConfig()
+
+  return {
+    db: useDb(db)
+  }
+}
+
+/*****************************************************************************************************************/
+
+export type Context = inferAsyncReturnType<typeof createContext>
+
+/*****************************************************************************************************************/
