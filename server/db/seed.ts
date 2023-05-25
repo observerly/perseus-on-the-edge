ddbea0b66a2bb091994bd07c6aab4d4403d784c7
Michael J. Roberts <michael@observerly.com>
2023-05-25 14:25:28 +0100

    feat: Added seed() composable to server/db/seed.
    feat: Added seed() composable to server/db/seed. Includes adding sample messier data. Includes adding "db:seed" pnpm script for "dotenv -e .env command -- tsx server/db/seed.ts".

---
+++ server/db/seed.ts
@@ -0,0 +1,67 @@
+/*****************************************************************************************************************/
+
+// @author         Michael Roberts <michael@observerly.com>
+// @package        @observerly/perseus-on-the-edge
+// @license        Copyright © 2021-2023 observerly
+
+/*****************************************************************************************************************/
+
+import { LibSQLDatabase } from 'drizzle-orm/libsql'
+
+import { LibsqlError } from '@libsql/client'
+
+import { useDb } from './utils'
+
+import { type Body } from './schema/bodies'
+
+import { upsertBodyByUID } from '../orm/bodies'
+
+import { messier } from '../../data/messier'
+
+/*****************************************************************************************************************/
+
+const db = useDb({
+  url: process.env.TURSO_DB_URL || 'file:db.perseus.sqlite',
+  authToken: process.env.TURSO_DB_AUTH_TOKEN
+})
+
+/*****************************************************************************************************************/
+
+const seedBodies = async (db: LibSQLDatabase, values: Body[], action?: string) => {
+  try {
+    values.forEach(async value => {
+      await upsertBodyByUID(db, value)
+    })
+  } catch (error) {
+    if (error instanceof LibsqlError) {
+      console.log(
+        `Something went wrong while seeding ${action || 'the bodies table'}. ${error.name}: ${
+          error.code
+        } ${error.message}`
+      )
+    } else {
+      console.log(`Something went wrong while seeding ${action || 'the bodies table'}.`)
+    }
+
+    console.log(`Seeding ${action || 'the bodies table'} finished unsuccessfully.`)
+    return
+  }
+
+  console.log(`Seeding ${action || 'the bodies table'} finished successfully.`)
+}
+
+/*****************************************************************************************************************/
+
+const seed = async (db: LibSQLDatabase) => {
+  // Seed the bodies table with Messier objects:
+  console.log('Seeding the bodies table with Messier objects...')
+  await seedBodies(db, messier, 'the bodies table with Messier objects')
+  console.log('Finished seeding the database.')
+}
+
+/*****************************************************************************************************************/
+
+console.log('Seeding the database...')
+seed(db)
+
+/*****************************************************************************************************************/
